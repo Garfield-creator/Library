88d46f40595c8024a941b20230c82b71d7397662
Mikael Nordman <mikael.nordman147@gmail.com>
2026-03-13 14:45:10 +0100

    Fixed apiUrl

--- library.client/src/environments/environment.prod.ts
+++ library.client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://MikaelRedCodeLibrary.azurewebsites.net/api'
+  apiUrl: 'https://happy-mushroom-057094103.4.azurestaticapps.net/'
 };
